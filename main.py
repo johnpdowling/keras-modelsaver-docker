3877d0ce2b7defe6a9bbf5176ac1f61f316c9a17
John Dowling <john.patrick.dowling@gmail.com>
2019-10-07 17:19:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -10,21 +10,34 @@ import os
 import time
 import uuid
 
-from keras.preprocessing.image import img_to_array
-from keras.applications import imagenet_utils
 import numpy as np
-from PIL import Image
-import redis
+
+import tensorflow as tf
+from tensorflow.python.framework import graph_io
+from tensorflow.keras.models import load_model
+import tensorflow.contrib.tensorrt as trt
 
 from fastapi import FastAPI, File, HTTPException
 from starlette.requests import Request
 
-os.makedirs('./model', exist_ok=True)
+model_folder = "/models"
+
+os.makedirs(model_folder, exist_ok=True)
 app = FastAPI()
 
 CLIENT_MAX_TRIES = int(os.environ.get("CLIENT_MAX_TRIES"))
 
+def save_model(model_name)
+    model = ResNet50(weights=model_name)
+    model.save(model_folder + "/" + model_name + ".h5"
 
+def freeze_graph(graph, session, output, save_pb_dir='.', save_pb_name='frozen_model.pb', save_pb_as_text=False):
+    with graph.as_default():
+        graphdef_inf = tf.graph_util.remove_training_nodes(graph.as_graph_def())
+        graphdef_frozen = tf.graph_util.convert_variables_to_constants(session, graphdef_inf, output)
+        graph_io.write_graph(graphdef_frozen, save_pb_dir, save_pb_name, as_text=save_pb_as_text)
+        return graphdef_frozen
+    
 @app.get("/")
 def index():
     return "Hello World!"
